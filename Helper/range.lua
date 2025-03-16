cfb77075a784abadf7d050e235127d2997517cc4
NAYOTO. <17558264+nayotodot@users.noreply.github.com>
2025-03-16 19:11:32 +0900

    Update range.lua

--- Helper/range.lua
+++ Helper/range.lua
@@ -1,15 +1,21 @@
-local epsilon = 0.000001;
+local function compare(step, idx, limit)
+	if 0 < step then
+		return idx < limit;
+	else
+		return limit < idx;
+	end
+end
 
-local function range(from, to, step)
+local function range(init, limit, step)
 	step = step or 1;
-	from = from - step;
-	to = to - epsilon;
-	return function()
-		from = from + step;
-		if from < to then
-			return from;
+	local idx = init - step;
+	local function iter()
+		idx = idx + step;
+		if compare(step, idx, limit) then
+			return idx;
 		end
 	end
+	return iter;
 end
 
 return range;
